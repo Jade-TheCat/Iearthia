90054d1de25134c152141b7e9f4be2d9025abe25
JasonIEC <jason.krabbe@gmail.com>
2018-03-19 19:49:13 -0500

    Apparently some things didn't get stafed
    Dangit, GitKraken!

--- src/main/java/iea/iearthia/block/BlockOreTitanium.java
+++ src/main/java/iea/iearthia/block/BlockOreTitanium.java
@@ -1,6 +1,6 @@
-package iea.iearthia.core.block;
+package iea.iearthia.block;
 
-import iea.iearthia.core.util.Constants;
+import iea.iearthia.util.Constants;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
--- src/main/java/iea/iearthia/block/IearthiaBlocks.java
+++ src/main/java/iea/iearthia/block/IearthiaBlocks.java
@@ -1,11 +1,11 @@
-package iea.iearthia.core.block;
+package iea.iearthia.block;
 
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class IearthiaBlocks {
-    @GameRegistry.ObjectHolder("iearthia-core:oretitanium")
+    @GameRegistry.ObjectHolder("iearthia:oretitanium")
     public static BlockOreTitanium oreTitanium;
 
     @SideOnly(Side.CLIENT)
